b694693e65aa688b166a5e0fb9240944e178e983
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-03 06:56:00 +0600

    first

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (org.apache.commons.lang3.StringUtils.isNumeric(str)) {
+            return !org.apache.commons.lang3.StringUtils.contains(str, "-");
+        }
+        return false;
     }
 }
